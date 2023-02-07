e14e980c3a92f35d53577648b9df29ee5124fbbb
Owen Garland <Ogarland9238@conestogac.on.ca>
2023-02-07 12:32:16 -0500

    fixed commits for hitler brodin

--- CSCN72040Assignment1Group7/src/com/ctrlaltdefeatA01/Data.java
+++ CSCN72040Assignment1Group7/src/com/ctrlaltdefeatA01/Data.java
@@ -6,60 +6,60 @@ public class Data {
 	double z;
 	int orientation;
 	
-	/// @brief This is a data default constructor
+	//This is a data default constructor
 	public Data() {
 		this.x = 0;
 		this.y = 0;
 		this.z = 0;
 		this.orientation = 0;
 	}
-	/// @brief parameterized Data constructor. 
+	//parameterized Data constructor. 
 	public Data(double x, double y, double z, int orientation) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.orientation = orientation;
 	}
-	/// @brief Data copy constructor
+	//Data copy constructor
 	public Data(Data data) { // copy constructor
 		this.x = data.x;
 		this.y = data.y;
 		this.z = data.z;
 		this.orientation = data.orientation;
 	}
-	/// @brief This is a getter that can get the x value of the phone 
+	//This is a getter that can get the x value of the phone 
 	public double getX() {
 		return x;
 	}
-	/// @brief This is a getter that can get the x value of the phone 
+	//This is a getter that can get the x value of the phone 
 	public void setX(double x) {
 		this.x = x;
 	}
-	/// @brief This is a getter that can get the y value of the phone 
+	//This is a getter that can get the y value of the phone 
 	public double getY() {
 		return y;
 	}
-	/// @brief This is a setter that can set the y value of the data set
+	//This is a setter that can set the y value of the data set
 	public void setY(double y) {
 		this.y = y;
 	}
-	/// @brief This is a getter that can get the z value of the phone
+	//This is a getter that can get the z value of the phone
 	public double getZ() {
 		return z;
 	}
-	/// @brief This is a setter that can set the z value of the data set
+	//This is a setter that can set the z value of the data set
 	public void setZ(double z) {
 		this.z = z;
 	}
-	/// @brief This is a getter that can get the orientation of the phone using a classifier 
+	//This is a getter that can get the orientation of the phone using a classifier 
 	public int getOrientation() {
 		return orientation;
 	}
-	/// @brief This is a setter that can set the orientation value of the data set
+	//This is a setter that can set the orientation value of the data set
 	public void setOrientation(int orientation) {
 		this.orientation = orientation;
 	}
-	/// @brief This is the equals method, sets the current object equal to parameter object
+	//This is the equals method, sets the current object equal to parameter object
 	public void equals(Data data) {
 		this.x = data.x;
 		this.y = data.y;
@@ -68,7 +68,7 @@ public class Data {
 		
 	}
 	
-	/// @brief This is a find distance function that will calculate the distance between two data points
+	//This is a find distance function that will calculate the distance between two data points
 	/// This function is overridden from the classifier class
 	public double findDistance(Data point2) {
 		return Math.sqrt((Math.pow(point2.getX() - this.getX(), 2)) + (Math.pow(point2.getY() - this.getY(), 2)) + (Math.pow(point2.getZ() - this.getZ(), 2)));
