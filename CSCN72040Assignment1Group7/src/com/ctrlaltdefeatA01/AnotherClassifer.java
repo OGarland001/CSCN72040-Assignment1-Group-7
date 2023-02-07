ce20875e1c070c9b3c930ba27cd361e2bdfd2b7c
Brodin Collins-Robb <tapeit03@outlook.com>
2023-02-07 11:59:24 -0500

    Wrote Classifier interface and wrote Another Classifier class

--- CSCN72040Assignment1Group7/src/com/ctrlaltdefeatA01/AnotherClassifer.java
+++ CSCN72040Assignment1Group7/src/com/ctrlaltdefeatA01/AnotherClassifer.java
@@ -1,5 +1,37 @@
 package com.ctrlaltdefeatA01;
 
-public class AnotherClassifer {
+class AnotherClassifer implements Classifer
+{
 
+	//This is a AnotherClassifiers constructor
+	public AnotherClassifer() {
+		// Print out what the constructor is doing
+		System.out.println("AnotherClassifier constructor called");
+	}
+	
+	//This is a classify method that will classify a phone orientation
+	//This is an overridden method that implements the anotherClassifer version of classify
+	@Override
+	public int classify(Data givenData) {
+		// Print out what the method is doing
+		System.out.println("AnotherClassifier constructor called");
+		return 0;
+	}
+
+	//This is a classify file method that will classify phone orientations in a given file
+	//This is an overridden method that implements the anotherClassifer version of train
+	@Override
+	public void train() {
+		// Print out what the method is doing
+		System.out.println("AnotherClassifier classify called");
+		
+	}
+
+	//This is a classify file method that will classify phone orientations in a given file	
+	//This is an overridden method that implements the anotherClassifer version of classifyFile
+	@Override
+	public void classifyFile(String filenameString) {
+		// Print out what the method is doing
+		System.out.println("AnotherClassifier classifyFile called");
+	}
 }
--- CSCN72040Assignment1Group7/src/com/ctrlaltdefeatA01/Classifer.java
+++ CSCN72040Assignment1Group7/src/com/ctrlaltdefeatA01/Classifer.java
@@ -1,5 +1,11 @@
 package com.ctrlaltdefeatA01;
 
-public class Classifer {
-
+interface Classifer 
+{
+	//This is a classify method that will classify a phone orientation
+	int classify(Data givenData);
+	//This is a classify file method that will classify phone orientations in a given file
+	void classifyFile(String filenameString);
+	//This is a train method that will train the algorithm
+	void train();
 }
