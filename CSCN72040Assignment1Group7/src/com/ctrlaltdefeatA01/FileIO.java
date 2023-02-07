ddc491f4c6f8d1f3f8ded0f8a1151f87ba3ed792
Tyler Scheifley <tylerscheifley@gmail.com>
2023-02-07 12:46:45 -0500

    FileIO complete

--- CSCN72040Assignment1Group7/src/com/ctrlaltdefeatA01/FileIO.java
+++ CSCN72040Assignment1Group7/src/com/ctrlaltdefeatA01/FileIO.java
@@ -2,4 +2,15 @@ package com.ctrlaltdefeatA01;
 
 public class FileIO {
 
+	protected String fileName;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	
 }
